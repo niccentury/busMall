6cc100bdfda6850b07b2b917d7e47364b91c4f9c
niccentury <cjdcec@gmail.com>
2018-08-14 16:35:56 -0700

    finished up mondays lab

--- app.js
+++ app.js
@@ -3,16 +3,22 @@
 function Products(filename) {
   this.filename = filename;
   this.votes = 0;
-  this.click = 0;
+  this.clicks = 0;
   this.timesDisplayed = 0;
   Products.allproducts.push(this);
 }
 Products.allproducts = [];
 
-//??????
-new Products('img/bag.jpg');
-new Products('img/banana.jpg');
-new Products('img/bathroom.jpg');
+var totalclicks = 0;
+
+var pastDisplayed = [];
+
+
+
+
+var products1 = new Products('img/bag.jpg');
+var products2 = new Products('img/banana.jpg');
+var products3 = new Products('img/bathroom.jpg');
 new Products('img/boots.jpg');
 new Products('img/breakfast.jpg');
 new Products('img/bubblegum.jpg');
@@ -33,21 +39,36 @@ new Products('img/wine-glass.jpg');
 
 console.log(Products.allproducts);
 
-//This is how to call three products, good
-var products1 = new Products('img/boots.jpg');
-var products2 = new Products('img/breakfast.jpg');
-var products3 = new Products('img/bubblegum.jpg');
+function displayed(){
+  products1.timesDisplayed++;
+  products2.timesDisplayed++;
+  products3.timesDisplayed++;
+}
 
 function displayThreeNewproducts() {
   // show new pictures to user
   // grab 2 productss at random
   do {
-    var firstProductIndex = Math.floor(Math.random() * Products.allproducts.length);
-    var secondProductsIndex = Math.floor(Math.random() * Products.allproducts.length);
-    var thirdProductsIndex = Math.floor(Math.random() * Products.allproducts.length);
-  } while (firstProductIndex === thirdProductsIndex);(firstProductIndex === secondProductsIndex);(secondProductsIndex === thirdProductsIndex);
-  //   products1 = Products.allproducts[];
-  //   products2 = Products.allproducts[];
+    var firstProductIndex = Math.floor(Math.random() * Products.allproducts.length);}
+  while (firstProductIndex === secondProductsIndex || firstProductIndex === thirdProductsIndex || pastDisplayed.includes(firstProductIndex));
+
+  do {var secondProductsIndex = Math.floor(Math.random() * Products.allproducts.length);}
+  while (secondProductsIndex === firstProductIndex || secondProductsIndex === thirdProductsIndex || pastDisplayed.includes(secondProductsIndex));
+
+  do {var thirdProductsIndex = Math.floor(Math.random() * Products.allproducts.length);}
+  while (thirdProductsIndex === firstProductIndex || thirdProductsIndex === secondProductsIndex || pastDisplayed.includes(thirdProductsIndex));
+
+  pastDisplayed[0] = firstProductIndex;
+  pastDisplayed[1] = secondProductsIndex;
+  pastDisplayed[2] = thirdProductsIndex;
+
+  displayed();
+
+  console.log(displayThreeNewproducts);
+
+
+  products1 = Products.allproducts[firstProductIndex];
+  products2 = Products.allproducts[secondProductsIndex];
   products3 = Products.allproducts[thirdProductsIndex];
 
   // change img src on the page to match the 3 new productss
@@ -56,6 +77,9 @@ function displayThreeNewproducts() {
   img3.src = products3.filename;
 }
 
+function stopEvent(){
+  document.getElementById('stop').setAttribute('style', 'display:none');
+}
 // event listeners
 // where are we listening? the images
 var img1 = document.getElementsByTagName('img')[0];
@@ -64,26 +88,44 @@ var img3 = document.getElementsByTagName('img')[2];
 // what are we listening for? click
 img1.addEventListener('click', function() {
   // add to votes for that Products
-  products1.votes++;
+  totalclicks++;
+  if (totalclicks < 25){
+    products1.votes++;
+    displayThreeNewproducts();
+  }
+  else{stopEvent();}
   // products1.votes = products1.votes + 1;
-  displayThreeNewproducts();
+
 });
 
 img2.addEventListener('click', function() {
   // what should we do?
   // add to votes for that Products
-  products2.votes++;
+  totalclicks++;
+  if (totalclicks < 25){
+    products2.votes++;
+
+    displayThreeNewproducts();
+  }
+  else{stopEvent();}
   // products1.votes = products1.votes + 1;
-  displayThreeNewproducts();
+
 });
 
 img3.addEventListener('click', function() {
   // what should we do?
   // add to votes for that Products
-  products3.votes++;
+  totalclicks++;
+  if (totalclicks < 25){
+    products3.votes++;
+    displayThreeNewproducts();
+  }
+  else{stopEvent();}
   // products1.votes = products1.votes + 1;
-  displayThreeNewproducts();
 });
+
+
+
 
 
 
